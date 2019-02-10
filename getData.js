f296d1641fbf684f34c751d5450b6613957f51c6
Martin Musale <martinmshale@gmail.com>
2019-02-11 02:13:55 +0300

    Script to create the songs.json data

---
+++ getData.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const readline = require("readline");
+const stream = require("stream");
+const db = require("./songs.json");
+const instream = fs.createReadStream("./songs.txt");
+const outstream = new stream();
+const rl = readline.createInterface(instream, outstream);
+
+const arr = [];
+
+rl.on("line", function(line) {
+  const data = line.split("-");
+  const artiste = data.shift().trim();
+  const newSong = data.shift().trim();
+  const songs = db[artiste];
+
+  if (songs !== undefined) {
+    if (!songs.includes(newSong)) db[artiste].push(newSong);
+  } else db[artiste] = [newSong];
+});
+
+rl.on("close", function() {
+  fs.writeFile("songs.json", JSON.stringify(db, null, 2), "utf-8", function(
+    err
+  ) {
+    if (err) console.error(err);
+    console.log("File updates");
+  });
+});
