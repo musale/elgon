8b12bcdd6a5a25f52fb52b9a79d3edf4583da65d
Martin Musale <martinmshale@gmail.com>
2019-02-11 02:52:02 +0300

    Tweet four times

--- lyrics.js
+++ lyrics.js
@@ -26,12 +26,14 @@ function getRandom(min, max) {
 }
 
 (async () => {
-  const artiste = pickRandomProperty(db);
-  const items = db[artiste];
-  const song = items[(items.length * Math.random()) | 0];
-  const lyricsUrl = encodeURI(`${lyricsBaseUrl}/${artiste}/${song}`);
+  for (let i = 0; i < lyricSize; i++) {
+    const artiste = pickRandomProperty(db);
+    const items = db[artiste];
+    const song = items[(items.length * Math.random()) | 0];
+    const lyricsUrl = encodeURI(`${lyricsBaseUrl}/${artiste}/${song}`);
 
-  tweetLyrics(lyricsUrl, song, artiste);
+    tweetLyrics(lyricsUrl, song, artiste);
+  }
 })();
 
 function tweetLyrics(lyricsUrl, song, artiste, retry = 0) {
@@ -56,7 +58,7 @@ function tweetLyrics(lyricsUrl, song, artiste, retry = 0) {
       tweetLyrics(lyricsUrl, song, artiste, retry);
     } else {
       const splitLyrics = lyrics.split("\n").filter(n => n);
-      const tweetStr = chopLyricsToTweet(splitLyrics);
+      const tweetStr = chopLyricsToTweet(splitLyrics, artiste);
       try {
         await client.post("statuses/update", { status: tweetStr });
         console.log(`Tweeted out that ${tweetStr}`);
@@ -64,16 +66,20 @@ function tweetLyrics(lyricsUrl, song, artiste, retry = 0) {
         console.log(`Error tweeting ${tweetStr}`);
         console.error(error);
       }
-      console.log(tweetStr);
       return;
     }
   });
 }
-function chopLyricsToTweet(splitLyrics, retry = 0) {
+function chopLyricsToTweet(splitLyrics, artiste, retry = 0) {
   if (retry >= lyricSize) return;
   const minRange = getRandom(0, splitLyrics.length - lyricSize);
   const tweetStrList = splitLyrics.slice(minRange, minRange + lyricSize);
-  const tweetStr = tweetStrList.join("\n");
+  // If I want to add hashtags
+  // const tweetStr = `${tweetStrList.join("\n")}\n#${artiste.replace(
+  //   /\s+/g,
+  //   ""
+  // )}`.trim();
+  const tweetStr = tweetStrList.join("\n").trim();
   if (tweetStr.length <= 280) return tweetStr;
-  else return chopLyricsToTweet(splitLyrics, retry + 1);
+  else return chopLyricsToTweet(splitLyrics, artiste, retry + 1);
 }
