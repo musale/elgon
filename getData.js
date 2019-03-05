7680d8df3432d258de1c681ad2ce44c8a0128413
Martin Musale <martinmshale@gmail.com>
2019-03-05 23:20:51 +0300

    Try/Catch reading of songs.txt as some data might be wrongly formatted

--- getData.js
+++ getData.js
@@ -10,13 +10,18 @@ const arr = [];
 
 rl.on("line", function(line) {
   const data = line.split("-");
-  const artiste = data.shift().trim();
-  const newSong = data.shift().trim();
-  const songs = db[artiste];
+  try {
+    const artiste = data.shift().trim();
+    const newSong = data.shift().trim();
+    const songs = db[artiste];
 
-  if (songs !== undefined) {
-    if (!songs.includes(newSong)) db[artiste].push(newSong);
-  } else db[artiste] = [newSong];
+    if (songs !== undefined) {
+      if (!songs.includes(newSong)) db[artiste].push(newSong);
+    } else db[artiste] = [newSong];
+  } catch (error) {
+    console.log(line);
+    console.error(error);
+  }
 });
 
 rl.on("close", function() {
